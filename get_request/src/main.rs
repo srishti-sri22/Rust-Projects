df271ab4ef81999eb00928a71acf5a94586fbb71
Srishti Srivastava <srishtisri224@gmail.com>
2025-12-03 17:58:49 +0530

    add the get_reqwest project

---
+++ get_request/src/main.rs
@@ -0,0 +1,21 @@
+use error_chain::error_chain;
+use std::io::Read;
+
+error_chain!{
+    foreign_links{
+        Io(std::io::Error);
+        HttpRequest(reqwest::Error);
+    }
+}
+
+
+fn main() -> Result<()> {
+    let mut res = reqwest::blocking::get("http://httpbin.org/get")?;
+    let mut body = String::new();
+    res.read_to_string(&mut body)?;
+
+    println!("Status of the code is : {}", res.status());
+    println!("Headers:{:#?}", res.headers());
+    println!("Body: {}", body);
+    Ok(())
+}
