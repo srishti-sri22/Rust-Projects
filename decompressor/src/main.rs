9f4cb4386ef048a3d0c8b42a5ae2d6d5b2bc4c78
Srishti Srivastava <srishtisri224@gmail.com>
2025-12-03 13:59:21 +0530

    corrected the files

--- decompressor/src/main.rs
+++ decompressor/src/main.rs
@@ -33,12 +33,15 @@ fn main() {
 
     let time2 = Instant::now();
 
-    let compressed_reader = File::open(&compressed_path).unwrap();
+    let compressed_reader = File::open(&compressed_path).unwrap(); //ek reader banaya hai for the given compressed path jo cli sse dia hai
+
     let mut decoder = GzDecoder::new(compressed_reader);
+    //ek decoder stream banayi hai to get the values and store them
     let mut decompressed_output = File::create(&decompressed_path).unwrap();
+    //ek file create kr rhe hai to store it the decompressed_output over there , will thr path jo hmne dia hai usko
 
     copy(&mut decoder, &mut decompressed_output).unwrap();
-
+    //decoder ka data output pr copy kr do
     println!("\n--- Decompression Done ---");
     println!("Compressed len: {}", File::open(&compressed_path).unwrap().metadata().unwrap().len());
     println!("Decompressed len: {}", File::open(&decompressed_path).unwrap().metadata().unwrap().len());
